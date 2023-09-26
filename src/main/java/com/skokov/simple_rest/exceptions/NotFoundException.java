2531f32838cf71e052ba51ac1adeadce06935f5c
skokovsv <sskokov007@gmail.com>
2023-09-27 00:05:20 +0300

    first

---
+++ src/main/java/com/skokov/simple_rest/exceptions/NotFoundException.java
@@ -0,0 +1,9 @@
+package com.skokov.simple_rest.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NotFoundException  extends RuntimeException{
+}
